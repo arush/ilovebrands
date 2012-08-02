b5abcd77800b86467c8b4c8f060b8bcafad6fe46
Arush Sehgal <arush@ilovebrands.net>
2012-08-02 20:39:21 +0100

    testing mixpanel

---
+++ app/code/local/Arush/Mixpanel/Helper/Data.php
@@ -0,0 +1,32 @@
+<?php
+
+class Arush_Mixpanel_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	/*
+	*
+	* Get the user's IP address and check against proxies
+	* Author: @ldn_tech_exec
+	* Based on code from http://thepcspy.com/read/getting_the_real_ip_of_your_users/
+	**/
+	public function getUserIp() {
+
+    if (isset($_SERVER)) {
+
+    	// if using a proxy
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+            return $_SERVER["HTTP_X_FORWARDED_FOR"];
+        
+        if (isset($_SERVER["HTTP_CLIENT_IP"]))
+            return $_SERVER["HTTP_CLIENT_IP"];
+
+        return $_SERVER["REMOTE_ADDR"];
+    }
+
+    if (getenv('HTTP_X_FORWARDED_FOR'))
+        return getenv('HTTP_X_FORWARDED_FOR');
+
+    if (getenv('HTTP_CLIENT_IP'))
+        return getenv('HTTP_CLIENT_IP');
+
+    return getenv('REMOTE_ADDR');
+}
